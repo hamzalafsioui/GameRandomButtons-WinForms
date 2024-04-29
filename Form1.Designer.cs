de7f4644074e69950f808ddd516cd2b842e4c2ee
hamzalafsioui <hamzalafsioui@gmail.com>
2024-04-29 12:19:16 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace GameRandomButtons
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblCounter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnStart.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(361, 185);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(101, 54);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblCounter
+            // 
+            this.lblCounter.AutoSize = true;
+            this.lblCounter.BackColor = System.Drawing.Color.Yellow;
+            this.lblCounter.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCounter.Location = new System.Drawing.Point(754, 22);
+            this.lblCounter.Name = "lblCounter";
+            this.lblCounter.Size = new System.Drawing.Size(24, 25);
+            this.lblCounter.TabIndex = 1;
+            this.lblCounter.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCounter);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblCounter;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace GameRandomButtons
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int Counter = 0;
+        bool IsOverGame = false;
+        int BtnSpeed = 5;
+        Random random = new Random();
+
+        void CreateRandomButton()
+        {
+            Button btn = new Button();
+            btn.Top = 0;
+            btn.Width = 100;
+            btn.Height = 30;
+            btn.Left = random.Next(0, ClientSize.Width - btn.Width);
+            btn.BackColor = Color.LightGoldenrodYellow;
+            btn.Text = random.Next(1, 100).ToString();
+            btn.Click += button_Click;
+            this.Controls.Add(btn);
+
+        }
+        void btnMoving(Button btn)
+        {
+            btn.Top += BtnSpeed;
+
+        }
+
+        void MoveAllButtons()
+        {
+            foreach (Button btn in this.Controls.OfType<Button>())
+            {
+                btnMoving(btn);
+
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            CreateRandomButton();
+            timer1.Start();
+            this.Controls.Remove((Button)sender);
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            this.Controls.Remove((Button)sender);
+            Counter++;
+            lblCounter.Text = Counter.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            CreateRandomButton();
+            MoveAllButtons();
+            if (GameEnd() == true)
+            {
+                return;
+            }
+        }
+        private void ResetForm()
+        {
+            // Reset counter
+            Counter = 0;
+            lblCounter.Text = "0";
+
+            // Remove all buttons from the form
+            foreach (Button btn in this.Controls.OfType<Button>().ToList())
+            {
+                this.Controls.Remove(btn);
+            }
+
+            // Reset game state
+            IsOverGame = false;
+            this.Controls.Add(btnStart);
+        }
+        bool GameEnd()
+        {
+            foreach (Button btn in Controls.OfType<Button>())
+            {
+                if (btn.Top == ClientSize.Height)
+                {
+                    timer1.Stop();
+                    MessageBox.Show("Game Over (-");
+                    // Reset the form
+                    ResetForm();
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
